df7beb5eb601c522247b57d948ad4a1f946594ca
eniyathirumalai <eniyathirumalai7@gmail.com>
2025-10-23 01:10:49 -0700

    Add files via upload

---
+++ firRW.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+int main(){
+	FILE*file;
+	char filename[]="example.txt";
+	char data[]="hello,this is a sample text written to the file.\n";
+	char buffer[256];
+	file=fopen(filename,"W");
+	if(file==NULL){
+		perror("Error opening file for writing");
+		return 1;
+	}
+	fprintf(file,"%s",data);
+	fclose(file);
+	printf("Data written to file successfully.\n");
+	file=fopen(filename,"r");
+	if (file==NULL){
+		perror("Error opening file for reading");
+		return 1;
+	}
+	printf("Contents of the file:\n");
+	while(fgets(buffer,sizeof(buffer),file)!=NULL){
+		printf("%s",buffer);
+	}
+	fclose(file);
+	return 0;
+}
